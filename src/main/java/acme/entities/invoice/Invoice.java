7962d4c99b32459ed1359d0f984126b208a97c88
Josué Rodríguez <josrodlop19@alum.us.es>
2024-03-04 23:30:44 +0100

    feature/Implements Invoice entity Ref #103

---
+++ src/main/java/acme/entities/invoice/Invoice.java
@@ -0,0 +1,70 @@
+
+package acme.entities.invoice;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.sponsorship.Sponsorship;
+
+public class Invoice {
+
+	// Serialization indentifier --------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Atributes ------------------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}", message = "{validation.claim.code}")
+	private String				code;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dueDate;
+
+	@NotNull
+	@Positive
+	private Double				quantity;
+
+	@NotNull
+	@Min(0)
+	private Double				tax;
+
+	@URL
+	private String				link;
+
+	// Derivated atributes --------------------------------------------------------------
+
+
+	public Double totalAmount() {
+		return this.quantity + this.tax * this.quantity;
+	}
+
+	// Relationships --------------------------------------------------------------------
+
+
+	@NotNull
+	@Valid
+	@ManyToOne()
+	private Sponsorship sponsorship;
+
+}
