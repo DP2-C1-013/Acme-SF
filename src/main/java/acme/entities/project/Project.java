33c1ba94147a79be9eb40d9f6e8334bb1dec93fa
davfuerod <davidfr0303@gmail.com>
2024-03-05 16:35:47 +0100

    feature/Implements Project entity Ref #115

---
+++ src/main/java/acme/entities/project/Project.java
@@ -0,0 +1,51 @@
+
+package acme.entities.project;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project extends AbstractEntity {
+
+	// Serialization indentifier --------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Atributes ------------------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{3}-[0-9]{4}")
+	private String				code;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				abstractText;
+
+	@NotNull
+	private Boolean				hasFatalErrors;
+
+	@NotNull
+	@Min(0)
+	private Double				cost;
+
+	@URL
+	private String				link;
+}
