c9d9312b3eba38cb3f4b8d23bb7bd07d23970849
OscarZurita <osczururp@alum.us.es>
2024-03-06 20:41:59 +0100

    Audit record withoud code audit

---
+++ src/main/java/acme/entities/auditrecord/AuditRecord.java
@@ -0,0 +1,55 @@
+
+package acme.entities.auditrecord;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditRecord {
+
+	//Serialization indentifier --------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Atributes ------------------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}", message = "{validation.auditRecord.code}")
+	private String				code;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startDate;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endDate;
+
+	@NotNull
+	private AuditRecordMark		mark;
+
+	@URL
+	private String				link;
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	private CodeAudit			codeAudit;
+}
