2776ddccd86ff3d7bc92ec568876e6a8e442bb9f
davfuerod <davidfr0303@gmail.com>
2024-03-07 11:48:46 +0100

    feature/Create sample data for project entity

--- src/main/java/acme/entities/project/Project.java
+++ src/main/java/acme/entities/project/Project.java
@@ -3,14 +3,16 @@ package acme.entities.project;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.datatypes.Money;
+import acme.client.data.datatypes.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -40,6 +42,8 @@ public class Project extends AbstractEntity {
 
 	private boolean				hasFatalErrors;
 
+	@NotNull
+	@Valid
 	private Money				cost;
 
 	private boolean				draftMode;
