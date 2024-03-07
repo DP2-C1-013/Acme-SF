eaa753691c529552080f7e7f1096a4caff361952
OscarZurita <osczururp@alum.us.es>
2024-03-07 15:57:02 +0100

    auditor role

---
+++ src/main/java/acme/roles/Auditor.java
@@ -0,0 +1,32 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Auditor {
+
+	@NotBlank
+	@Size(max = 75)
+	private String	firm;
+
+	@NotBlank
+	@Size(max = 25)
+	private String	professionalId;
+
+	@NotBlank
+	@Size(max = 100)
+	private String	certifications;
+
+	@URL
+	private String	link;
+}
