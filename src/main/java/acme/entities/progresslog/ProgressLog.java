2aca6e898e61d37e5cbe744c931deaa9604670a5
Migueel0 <miggaller@alum.us.es>
2024-03-08 14:33:13 +0100

    feature/Implement ProgressLog entity

---
+++ src/main/java/acme/entities/progresslog/ProgressLog.java
@@ -0,0 +1,64 @@
+
+package acme.entities.progresslog;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.contract.Contract;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ProgressLog extends AbstractEntity {
+
+	//Serialization indentifier --------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Atributes ------------------------------------------------------------------------
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,2}-[0-9]{3}")
+	private String				recordId;
+
+	@NotNull
+	@Min(0)
+	private Double				completeness;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				comment;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				responsiblePerson;
+
+	// Relationships --------------------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne()
+	private Contract			contract;
+
+}
