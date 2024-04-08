a9ec1931699cc4f3b5e50bfc90f36da687b54f16
Josué Rodríguez <josrodlop19@alum.us.es>
2024-04-08 13:27:05 +0200

    feature/Implements list operation for sponsors over sponsorships Ref #284

---
+++ src/main/java/acme/features/sponsor/SponsorSponsorshipShowService.java
@@ -0,0 +1,53 @@
+
+package acme.features.sponsor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorship.Sponsorship;
+import acme.roles.Sponsor;
+
+@Service
+public class SponsorSponsorshipShowService extends AbstractService<Sponsor, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SponsorSponsorshipRepository repository;
+
+	// AbstractService<Authenticated, Claim> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = super.getRequest().getPrincipal().hasRole(Sponsor.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Sponsorship object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneSponsorshipById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final Sponsorship object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "moment", "duration", "amount", "type", "email", "link", "draftMode");
+
+		super.getResponse().addData(dataset);
+	}
+}
