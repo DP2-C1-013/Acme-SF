208b9c091fe6d6e00e9e4bad4ac308bebe792804
antjimort <antjimort@alum.us.es>
2024-04-10 10:25:21 +0200

    feature/List banners and show details (backend)

---
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerListService.java
@@ -0,0 +1,47 @@
+
+package acme.features.administrator.banner;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Administrator;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.banner.Banner;
+
+@Service
+public class AdministratorBannerListService extends AbstractService<Administrator, Banner> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorBannerRepository repository;
+
+	// AbstractService<Administrator, Banner> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Banner> objects;
+		objects = this.repository.findAllBanners();
+		super.getBuffer().addData(objects);
+	}
+
+	@Override
+	public void unbind(final Banner object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "instantiationMoment", "startDisplay", "endDisplay");
+
+		super.getResponse().addData(dataset);
+	}
+}
