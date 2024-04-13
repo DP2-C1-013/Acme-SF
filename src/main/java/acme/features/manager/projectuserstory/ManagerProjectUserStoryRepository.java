0dcfd8cbbd82057c338efb337c1196a85578c596
davfuerod <davidfr0303@gmail.com>
2024-04-13 17:39:44 +0200

    Create User Story fixed

---
+++ src/main/java/acme/features/manager/projectuserstory/ManagerProjectUserStoryRepository.java
@@ -0,0 +1,10 @@
+package acme.features.manager.projectuserstory;
+
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface ManagerProjectUserStoryRepository extends AbstractRepository{
+
+}
