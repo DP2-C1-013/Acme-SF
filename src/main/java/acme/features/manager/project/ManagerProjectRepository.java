6377c2fe8bdbf2cf9e1f86b8f0ffa1d1e4bbfa08
davfuerod <davidfr0303@gmail.com>
2024-04-13 16:38:20 +0200

    Manager project list fixed

--- src/main/java/acme/features/manager/project/ManagerProjectRepository.java
+++ src/main/java/acme/features/manager/project/ManagerProjectRepository.java
@@ -1,6 +1,8 @@
 
 package acme.features.manager.project;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +13,13 @@ import acme.roles.Manager;
 @Repository
 public interface ManagerProjectRepository extends AbstractRepository {
 
-	@Query("SELECT m FROM Manager m WHERE m.userAccount.id = :id")
-	Manager findOneManagerByUserAccountId(int id);
+	@Query("SELECT m FROM Manager m WHERE m.id = :id")
+	Manager findOneManagerById(int id);
 
 	@Query("SELECT p FROM Project p WHERE p.id = :id")
 	Project findOneProjectById(int id);
+
+	@Query("SELECT DISTINCT pus.project FROM UserStory us LEFT JOIN ProjectUserStory pus ON us.id = pus.userStory.id WHERE us.manager.id = :id ")
+	Collection<Project> findCreatedProjectsByManagerId(int id);
 
 }
