b2f9bd0b57f2c15cf8218c559ae4f74c5fbc160e
davfuerod <davidfr0303@gmail.com>
2024-04-14 15:29:08 +0200

    Update user story working

--- src/main/java/acme/features/manager/userstory/ManagerUserStoryController.java
+++ src/main/java/acme/features/manager/userstory/ManagerUserStoryController.java
@@ -25,6 +25,9 @@ public class ManagerUserStoryController extends AbstractController<Manager, User
 	private ManagerUserStoryCreateService	createService;
 
 	@Autowired
+	private ManagerUserStoryUpdateService	updateService;
+
+	@Autowired
 	private ManagerUserStoryPublishService	publishService;
 
 	// Constructors -----------------------------------------------------------
@@ -35,6 +38,7 @@ public class ManagerUserStoryController extends AbstractController<Manager, User
 		super.addBasicCommand("list", this.listService);
 		super.addBasicCommand("show", this.showService);
 		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
 
 		super.addCustomCommand("publish", "update", this.publishService);
 	}
