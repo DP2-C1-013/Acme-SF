13cd2c7aeaa370993dc0ec73d6aad6f13fbe0998
davfuerod <davidfr0303@gmail.com>
2024-04-19 18:33:16 +0200

    feature/Show project on principals

--- src/main/java/acme/features/authenticated/project/AuthenticatedProjectController.java
+++ src/main/java/acme/features/authenticated/project/AuthenticatedProjectController.java
@@ -16,7 +16,10 @@ public class AuthenticatedProjectController extends AbstractController<Authentic
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	private AuthenticatedProjectListService listService;
+	private AuthenticatedProjectListService	listService;
+
+	@Autowired
+	private AuthenticatedProjectShowService	showService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -24,5 +27,6 @@ public class AuthenticatedProjectController extends AbstractController<Authentic
 	@PostConstruct
 	protected void initialise() {
 		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
 	}
 }
