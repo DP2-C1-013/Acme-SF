e9ff3e997f6d0c83e6bff13c15ef5caad51412c3
Josué Rodríguez <josrodlop19@alum.us.es>
2024-04-25 20:02:49 +0200

    feature/Implements sponsorship list for any principal Ref #294

---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipListPublishedService.java
@@ -0,0 +1,51 @@
+
+package acme.features.any.sponsorship;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Any;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorship.Sponsorship;
+
+@Service
+public class AnySponsorshipListPublishedService extends AbstractService<Any, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AnySponsorshipRepository repository;
+
+	// AbstractService<Sponsor, Sponsorship> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Sponsorship> objects;
+
+		objects = this.repository.findPublishedSponsorships();
+
+		super.getBuffer().addData(objects);
+	}
+
+	@Override
+	public void unbind(final Sponsorship object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "moment", "type");
+		dataset.put("project", object.getProject().getCode());
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipShowService.java
@@ -0,0 +1,13 @@
+
+package acme.features.any.sponsorship;
+
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Any;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorship.Sponsorship;
+
+@Service
+public class AnySponsorshipShowService extends AbstractService<Any, Sponsorship> {
+
+}
