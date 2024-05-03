b07915b30b92d717bedf4a281f031355a2eeadf5
antjimort <antjimort@alum.us.es>
2024-05-03 10:07:16 +0200

    feature/list and show published training modules for authenticated

---
+++ src/main/java/acme/features/authenticated/trainingmodule/AuthenticatedTrainingModuleListService.java
@@ -0,0 +1,51 @@
+
+package acme.features.authenticated.trainingmodule;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Authenticated;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.trainingmodule.TrainingModule;
+
+@Service
+public class AuthenticatedTrainingModuleListService extends AbstractService<Authenticated, TrainingModule> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedTrainingModuleRepository repository;
+
+	// AbstractService<Authenticated, TrainingModule> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = super.getRequest().getPrincipal().hasRole(Authenticated.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Collection<TrainingModule> objects;
+		objects = this.repository.findAllPublishedTrainingModule();
+		super.getBuffer().addData(objects);
+	}
+
+	@Override
+	public void unbind(final TrainingModule object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "creationMoment", "updateMoment", "difficultyLevel", "draftMode");
+
+		super.getResponse().addData(dataset);
+	}
+}
---
+++ src/main/java/acme/features/authenticated/trainingmodule/AuthenticatedTrainingModuleShowService.java
@@ -0,0 +1,53 @@
+
+package acme.features.authenticated.trainingmodule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Authenticated;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.trainingmodule.TrainingModule;
+
+@Service
+public class AuthenticatedTrainingModuleShowService extends AbstractService<Authenticated, TrainingModule> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedTrainingModuleRepository repository;
+
+	// AbstractService<Authenticated, TrainingModule> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = super.getRequest().getPrincipal().hasRole(Authenticated.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		TrainingModule object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findTrainingModuleById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final TrainingModule object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "creationMoment", "details", "difficultyLevel", "updateMoment", "optionalLink", "estimatedTotalTime", "draftMode", "project", "developer");
+
+		super.getResponse().addData(dataset);
+	}
+}
