585a30eec454931b96985d887f56e9205389294a
davfuerod <davidfr0303@gmail.com>
2024-05-26 11:30:48 +0200

    Add functional tests student 1

--- src/main/java/acme/features/manager/userstory/ManagerUserStoryListMineService.java
+++ src/main/java/acme/features/manager/userstory/ManagerUserStoryListMineService.java
@@ -6,10 +6,8 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.client.data.accounts.Principal;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
-import acme.entities.project.Project;
 import acme.entities.userstory.UserStory;
 import acme.roles.Manager;
 
@@ -26,15 +24,8 @@ public class ManagerUserStoryListMineService extends AbstractService<Manager, Us
 
 	@Override
 	public void authorise() {
-		final int projectId = super.getRequest().getData("projectId", int.class);
-		Project project = this.repository.findOneProjectById(projectId);
+		super.getResponse().setAuthorised(super.getRequest().getPrincipal().hasRole(Manager.class));
 
-		final Principal principal = super.getRequest().getPrincipal();
-		final int userAccountId = principal.getAccountId();
-
-		final boolean authorise = project != null && principal.hasRole(Manager.class) && project.getManager().getUserAccount().getId() == userAccountId;
-
-		super.getResponse().setAuthorised(authorise);
 	}
 
 	@Override
